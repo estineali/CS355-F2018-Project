6ddb368ecce30e9fd602f0cc0455e567c3d89644
AlinaQureshi <31242900+AlinaQureshi@users.noreply.github.com>
2018-11-19 15:52:34 +0500

    Add files via upload

---
+++ Desktop/Admin_Doc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Pending_Orders PO = new Pending_Orders();
+            PO.Show();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            All_Orders AO = new All_Orders();
+            AO.Show();
+        }
+    }
+}
