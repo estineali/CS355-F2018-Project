7f1ed372adb0aa00e18fbe9132d13b7adc833912
Muhammad Shahrom Ali <estineali@gmail.com>
2018-12-05 14:32:33 +0500

    Database Population

---
+++ Population-Users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users values
+('System', 'Admin', 'sysadmin', 'administer', '2018-09-09', 'redroastpar@gmail.com', 1),  
+('Cafe2', 	'Go', 	'cafe2go', 	'c2gHUcafe2', '2015-01-15', 'cafe2go@habib.edu.pk', 1), 
+('Cafeela',	'Dhaba', 'cafeela',	'cdHUcafe3', '2018-01-01', 'cafeela@habib.edu.pk', 1),
+('Alina', 'Qureshi', 'aq03798', 'yomamasofat', '1999-07-03', 'aq03798@st.habib.edu.pk', 0),
+('Ali Mohammed', 'Shujjat', 'as03856', 'harubharu',	'1998-06-19',	'as03856@st.habib.edu.pk',	0),
+('Muhammad Shahrom', 'Ali', 'ma03559', 	'manamejeff', '1998-09-15', 'ma03559@st.habib.edu.pk', 0),
+('Sarah', 'Khan', 'sk03465', 'motherrussia', '1999-11-20', 'sk03465@st.habib.edu.pk', 0),
+('Aymen', 'Wasim', 'aq03452', 'harrypotterlav',	'1999-04-11',	'aq03452@st.habib.edu.pk',	0),
+('Muhammad', 'Shahzain', 'ms03977',	'arayshazen', '1999-03-25', 'ms03977@st.habib.edu.pk', 0),
+('Osama', 'Majeed', 'gr81000',	'verschluss', '1999-08-21', 'gr81000@gmail.com', 0)
