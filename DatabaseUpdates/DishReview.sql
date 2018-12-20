c31c50a3aa42cf5a03b6d5439bb81664ebb7d73a
Muhammad Shahrom Ali <estineali@gmail.com>
2018-12-20 14:42:24 +0500

    Add files via upload

---
+++ DatabaseUpdates/DishReview.sql
@@ -0,0 +1,9 @@
+SELECT * FROM DishReview
+
+INSERT INTO DishReview VALUES
+(71, 'C15', '2018-10-13 00:00:00.000', 'The drinks are very chilled everytime.', 5),
+(62, 'C1', CURRENT_TIMESTAMP, 'Delicious banana shake. Could do with less milk, though.', 3),
+(11, 'C2G11', CURRENT_TIMESTAMP, 'Too much salt. The patty was breaking. I have never enjoyed a burger less', 1),
+(9, 'C14', CURRENT_TIMESTAMP, 'Good tea but too strong.', 3),
+(11, 'C2G18', CURRENT_TIMESTAMP, 'Damn good fries! and I LOVEE the sauce', 5),
+(10, 'C2G23', CURRENT_TIMESTAMP, 'Glad the spaghetti were not soggy. Generous amounts of cheese. Loved it', 4);
