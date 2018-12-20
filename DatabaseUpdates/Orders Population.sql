6447070e7abe0a6df0be8a4f908f02e531884d19
Muhammad Shahrom Ali <estineali@gmail.com>
2018-12-20 17:59:26 +0500

    Add files via upload

---
+++ DatabaseUpdates/Orders Population.sql
@@ -0,0 +1,14 @@
+
+insert into Orders
+Values (11, 1, CURRENT_TIMESTAMP, 50, 'Rejected', 'Item Out of Stock', NULL, 1),
+(66, 1, CURRENT_TIMESTAMP, 80, 'Rejected', 'Item Out of Stock', NULL, 1),
+(10, 1, CURRENT_TIMESTAMP, 50, 'Rejected', 'Item Out of Stock', NULL, 1),
+(9, 1, CURRENT_TIMESTAMP, 110, 'Rejected', 'Item Out of Stock', NULL, 1),
+(12, 1, CURRENT_TIMESTAMP, 70, 'Rejected', 'Item Out of Stock', NULL, 1),
+(14, 1, CURRENT_TIMESTAMP, 70, 'Rejected', 'Item Out of Stock', NULL, 1),
+(11, 1, CURRENT_TIMESTAMP, 160, 'Pending', NULL, NULL, 1), --2 chai 1 brownie
+(66, 1, CURRENT_TIMESTAMP, 60, 'Pending', NULL, NULL, 1), --3 Samosay
+(10, 1, CURRENT_TIMESTAMP, 80, 'Pending', NULL, NULL, 1), --Beans
+(9, 1, CURRENT_TIMESTAMP, 110, 'Pending', NULL, NULL, 1), -- qorma
+(12, 1, CURRENT_TIMESTAMP, 70, 'Pending', NULL, NULL, 1), --Coffee with sugar
+(14, 1, CURRENT_TIMESTAMP, 70, 'Pending', NULL, NULL, 1) -- 2 Samosay 3 chips
