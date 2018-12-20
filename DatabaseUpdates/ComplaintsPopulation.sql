aa7726f5b364f87223bf20aec43e650a790446ba
Muhammad Shahrom Ali <estineali@gmail.com>
2018-12-20 14:39:13 +0500

    Create ComplaintsPopulation.sql

---
+++ DatabaseUpdates/ComplaintsPopulation.sql
@@ -0,0 +1,11 @@
+insert into Complaints values 
+(5, 'Test Complaint. All clear.', 1),
+(6, 'Customer Orders not reaching', 1),
+(7, 'Customer Orders not reaching', 1),
+(8, 'Customer Orders not reaching', 1),
+(5, 'Customer orders issue has been resolved', 1),
+(6, 'Can not login to Account', 0),
+(5, 'Tapal Cafeteria Login issue unsolved.', 0),
+(7, 'All menu items not showing', 0),
+(8, 'Customer Orders not reaching', 1),
+(5, 'Cafe2Go bug unsolved. Cafeela Bug solved.', 1)
